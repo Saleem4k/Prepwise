30cfa481c2c85391ee06513fa901f4307057665b
Saleem4k <mdsaleem1234567809@gmail.com>
2025-09-28 16:49:25 +0530

    reset config

--- firebase/client.ts
+++ firebase/client.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWkvI-sy8DPgyj3jgQAxVPW44zkTz3OYI",
-  authDomain: "prepwise-eca62.firebaseapp.com",
-  projectId: "prepwise-eca62",
-  storageBucket: "prepwise-eca62.firebasestorage.app",
-  messagingSenderId: "844118503236",
-  appId: "1:844118503236:web:221450baccaa210b43b111",
-  measurementId: "G-2N3Z1GD5GE",
+  apiKey: "AIzaSyAV8TH8K8eBsrTfPDJEKjaTNZw4tKkzt5k",
+  authDomain: "prepwise-9209e.firebaseapp.com",
+  projectId: "prepwise-9209e",
+  storageBucket: "prepwise-9209e.firebasestorage.app",
+  messagingSenderId: "1072038380230",
+  appId: "1:1072038380230:web:0a5697e60233b0b9b54dc7",
+  measurementId: "G-8JH80CK4RF"
 };
 
 // Initialize Firebase
